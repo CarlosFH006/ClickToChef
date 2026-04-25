370652512609449b623732ee254b1723ca94141d
Carlos <carferher8@alu.edu.gva.es>
2026-04-25 15:51:20 +0200

    Fix: Se cerraba la conexion a la BD en IngredientesDAO y RecetasDAO

--- ClickToChef-Server/src/main/java/org/example/DAO/RecetasDAO.java
+++ ClickToChef-Server/src/main/java/org/example/DAO/RecetasDAO.java
@@ -14,9 +14,9 @@ public class RecetasDAO {
     public static boolean insertarReceta(Recetas receta) {
         String sql = "INSERT INTO recetas (producto_id, ingrediente_id, cantidad_necesaria) VALUES (?, ?, ?)";
 
-        try (Connection conexion = ConexionDB.getConexion();
-             PreparedStatement statement = conexion.prepareStatement(sql)) {
-
+        try {
+            Connection conexion = ConexionDB.getConexion();
+            PreparedStatement statement = conexion.prepareStatement(sql);
             statement.setInt(1, receta.getProductoId());
             statement.setInt(2, receta.getIngredienteId());
             statement.setDouble(3, receta.getCantidadNecesaria());
@@ -30,10 +30,10 @@ public class RecetasDAO {
         String sql = "SELECT producto_id, ingrediente_id, cantidad_necesaria FROM recetas";
         ArrayList<Recetas> recetas = new ArrayList<>();
 
-        try (Connection conexion = ConexionDB.getConexion();
-             PreparedStatement statement = conexion.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
-
+        try {
+            Connection conexion = ConexionDB.getConexion();
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 Recetas receta = new Recetas(
                         resultSet.getInt("producto_id"),
